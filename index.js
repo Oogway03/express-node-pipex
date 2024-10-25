cbee96082097b5c861a905ee15d214088b518d07
Oogway03 <nmola2003@gmail.com>
2024-10-25 17:25:47 +0200

    fix: new endpoint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Endpoint API
-app.get("/api/data", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
 
