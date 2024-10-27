50c43b9394740dc647565b4a8e44aa9fcfc49604
Oogway <nmola2003@gmail.com>
2024-10-27 11:53:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 // Endpoint API
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express!" });
-});
-
-app.get("/marco", (req, res) => {
-  res.json({ message: "Mandami altri sticker!" });
+  res.json({ message: "Come lo se ciama il progetto?" });
 });
 
 app.listen(PORT, () => {
